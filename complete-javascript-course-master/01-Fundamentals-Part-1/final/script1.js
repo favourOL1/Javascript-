047754d416b3b95d783075d003246c769955153e
Favour <favourOL1@outlook.com>
2024-04-26 08:49:17 +0100

    first commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/final/script1.js
@@ -0,0 +1,196 @@
+/*
+// linking a javascript files
+
+// let js='amazing';
+//       if (js==='amazing') alert('javascript is FUN!');
+
+//       40 + 8 + 23 - 10;
+//       console.log( 40 + 8 + 23 - 10)
+
+//values and variables
+
+console.log('jonas')
+console.log(23)
+
+let firstName='favour'
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let my_name='favour' //or
+let myName='favour'
+
+let myFirstJob= 'Programmer'
+let myCurrentJob ='Teacher'
+
+let job1='programmer'
+let job2='teacher'
+
+console.log()
+
+        //ASS1....(A). Declare variables called country, continent and population and assign their
+         //values according to your own country (population in millions). (b) Log their values to the console.
+        
+let country = 'Nigeria'
+let continent = 'Africa'
+let population = '10'
+
+console.log( country)
+console.log(continent)
+console.log(population)
+
+        // DATA TYPES
+        //Object or 
+        //Primitive Data type
+
+        let javascriptIsFun = true;
+        console.log(javascriptIsFun)
+
+        console.log(typeof true);
+        console.log(typeof javascriptIsFun);
+        console.log( typeof 23);
+        console.log(typeof 'john')
+
+        //Dynamic data type -is re-asigning variables
+
+        javascriptIsFun ='YES'
+        console.log(typeof javascriptIsFun);
+
+        //converting an undefine variables to define variables
+
+        let year;
+        console.log(year)
+        console.log(typeof year)
+
+        //re-asigning the undefine variables
+
+        year = 1999;
+        console.log(typeof year)
+
+        console.log(typeof null); //is undefined
+     
+
+        //THREE TYPE OF DECLARING VARIABLES
+
+        //LET-CONST-VAR
+
+        let age = 25;
+        age = 27
+
+        const birthYear = 1999;
+        // birthYear=1998;
+
+        var job = 'teacher';
+        job = 'programmer';
+        
+
+       //Basic Operators in Javascript
+
+       // Mathematic/Arithematics  Operators- +,-,*
+
+       const year = 2024;
+       const ageJohn = year - 1999;
+       const ageFavour = year - 1997;
+       console.log(ageJohn, ageFavour)
+
+       console.log(ageJohn + 2, ageFavour / 10, 2**3);
+
+       const firstName = 'Favour';
+       const lastName = 'OL';
+       console.log( firstName + ' ' + lastName);
+
+       //Assignment operator- =
+
+       let X = 10 + 5; // x = 15
+       X += 10; // x = x + 10 = 25 
+       X *= 4; // x = x * 4 = 100
+       X ++;
+       X--;
+       console.log(X)
+
+       //Comparison operator- produce a boelen value
+
+       console.log( ageJohn < ageFavour);
+       console.log(ageFavour >= 18);
+
+       const isFullAge = ageFavour <= 18;
+
+       // Precedence of operator 
+
+       let x,y;
+       x = y = 25 - 10 - 5; //x = y = 10
+       console.log(x,y)
+
+       //pretences in operator
+
+       const aveageAge = (ageFavour + ageJohn) / 2;
+       console.log(ageFavour, ageJohn,  aveageAge);
+      
+
+       // Exercise
+
+        const massMark = 78;
+        const heightMark = 1.69;
+
+        const BMIMark= (massMark / (heightMark * heightMark));
+
+        console.log(BMIMark);
+
+        const massJohn = 92;
+        const heightJohn = 1.95;
+
+        const BMIJohn= (massJohn / (heightJohn * heightJohn));
+
+        console.log(BMIMark);
+
+        let markHigherBMI = true;
+
+        console.log(typeof markHigherBMI);
+
+        markHigherBMI = (BMIMark < BMIJohn );
+
+        console.log(markHigherBMI)
+         */
+
+        //Strings and Template literals
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
