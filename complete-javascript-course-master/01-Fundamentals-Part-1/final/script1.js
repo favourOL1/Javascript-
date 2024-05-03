44a12e53290d8989116d415cb684d3d134bce4cb
Favour <favourOL1@outlook.com>
2024-05-03 08:01:45 +0100

    first commit

--- complete-javascript-course-master/01-Fundamentals-Part-1/final/script1.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/final/script1.js
@@ -150,10 +150,293 @@ console.log(population)
         markHigherBMI = (BMIMark < BMIJohn );
 
         console.log(markHigherBMI)
-         */
+ 
 
         //Strings and Template literals
         
+        const firstName = 'favour';
+        const job ='programmer';
+        const birthYear = '1999';
+        const year = '2024'
+
+        const favour = " i'm " + firstName +' a ' + (year - 
+                birthYear) + ' years '+' old ' + job;
+
+        console.log(favour);
+
+
+         //Writing a template literals
+
+         const favourNew = `I'm ${firstName}, a ${year - birthYear} year old  ${job}`
+         console.log(favourNew)
+
+         console.log(`just a regular string......`)
+
+         //strings on multiple lines
+
+         console.log(`strings with \n\ 
+         multiple \n\
+         lines`);
+
+
+         //taking decision IF/Else statement
+
+         const age = 15;
+
+         if( age >= 18 ){
+                console.log('favour is old enough to have a driving lisence')
+         } else{
+                const yearLeft = 18 - age;
+                console.log(`john is too young. Wait another ${yearLeft} years :)`)
+         }
+
+         const  birthYear = 1998;
+         let centuery;
+
+         if (birthYear <= 2000){
+                centuery = 20;
+         }else {
+                centuery = 21;
+         }
+
+         console.log(centuery)
+
+
+
+         //If / else controll structure
+
+        //  if(){
+
+        //  }else{
+
+        //  }
+
+
+        //Exercise
+
+        
+        const massMark = 78;
+        const heightMark = 1.69;
+
+        const BMIMark= (massMark / (heightMark * heightMark));
+
+        console.log(BMIMark);
+
+        const massJohn = 92;
+        const heightJohn = 1.95;
+
+        const BMIJohn= (massJohn / (heightJohn * heightJohn));
+
+        console.log(BMIMark);
+
+        let markHigherBMI = true;
+
+        console.log(typeof markHigherBMI);
+
+        markHigherBMI = (BMIMark < BMIJohn );
+
+        console.log(markHigherBMI)
+
+        // if(BMIMark > BMIJohn){
+        //     console.log("Mark's BMI is higher than John's!");
+        // }else{
+        //     console.log("John's BMI is higher than Mark's!");
+        // }
+
+        if (BMIJohn >  BMIMark){
+                console.log( ` John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+        }else{
+                console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+        }
+
+        //Value type of conversion
+
+        const inputYear = '1999';
+        console.log(Number(inputYear), inputYear);
+
+        console.log(Number(inputYear) + 18 );
+
+        console.log(Number('john'))
+        console.log(typeof (NaN))// inavlid number
+
+        console.log(String(23), 23);
+
+         //Value type of coersion
+
+        console.log('i am ' + 23 + ' year old ');
+        console.log('23'- '10'- 3 );
+        console.log('23'* 2 );
+        console.log('23' / '2');
+
+        let n = '1' + 1;//  '11' bcus '1' is a string 
+        n = n - 1;
+        console.log(n) // 10(Number)
+       
+
+        //Truthy and Falsy Values
+        // 5 falsy values; '0', '', undefined, NaN, Null.
+
+        console.log(Boolean(0));
+        console.log(Boolean(undefined));
+        console.log(Boolean('john'));
+        console.log(Boolean({}));  // obj
+        console.log(Boolean(''));
+
+        const money = 100;
+        if (money){
+                console.log("Don't Spend it all");
+        } else{
+                console.log("You should get a Job");
+        }
+
+        let height = 123 ;
+        if (height){
+                console.log(" YAY! height is defined")
+        } else{
+                console.log("Height is undefined")
+        }
+       
+
+        //Equality Operators:
+
+        const age = '18';
+        if (age === 18) console.log("You just became an Adult (strict)" );
+
+        if (age == 18) console.log("You just became an Adult (loosed)" );
+
+        const favourite = Number( prompt (" What's your favourite value"))
+        console.log(favourite)
+        console.log( typeof favourite)
+
+        if (favourite === 25){
+                console.log('Cool! 25 is an amazing number')
+        } else if (favourite === 7){
+                console.log(" 7 is also a cool number ")
+        } else if ( favourite === 9){
+                console.log("9 is also a cool number")
+        }
+         else{
+                console.log(" Number is not 25 or 7 or 9  ")
+        }
+
+        if (favourite != 25 ) console.log("Why not 25?")
+        
+
+        //Boolean Logic
+        // basic Boolean operators: And, Or & Not Operators
+
+        // logical Operator
+
+        const hasDriverLisences = true ; //A
+        const hasGoodVission = true;// B
+
+        console.log(hasDriverLisences && hasGoodVission);
+        console.log(hasDriverLisences || hasGoodVission);
+        console.log(!hasDriverLisences);
+
+        // if(hasDriverLisences && hasGoodVission){
+        //         console.log("john is able to drive");
+        // }else{
+        //         console.log("someone else should drive ");
+        // }
+
+        const isTired = true;
+        console.log( hasDriverLisences && hasGoodVission && isTired)
+
+        if (hasDriverLisences && hasGoodVission && isTired){
+                console.log("john is able to drive");
+        }else {
+                console.log("someone else should drive ");
+        }
+       
+
+        // The switch statement
+
+        const day ='tuesday';
+
+        switch( day){
+                case 'monday':
+                        console.log("plan course structure");
+                        console.log("Go to coding meetup");
+                        break;
+                case 'tuesday':
+                        console.log('Prepare theory videos');
+                        break;
+                case 'wednesday':
+                case 'thursday':
+                        console.log('write code example');
+                        break;
+                case 'friday':
+                        console.log('Record vidoes');
+                        break;
+                case 'saturday':
+                case 'sunday':
+                        console.log('Enjoy the weekend');
+                        break;
+                default:
+                        console.log('Not a valid day')
+        }
+
+        // Using the if & else statement
+
+        if( day === 'monday'){
+                console.log("plan course structure");
+                console.log("Go to coding meetup");
+
+        }else if (day === 'tuesday'){
+                console.log('Prepare theory videos');
+
+        }else if (day === 'wednesday' || day === 'thursday'){
+                console.log('write code example');
+
+        }else if (day === 'friday'){
+                console.log('Record vidoes');
+                 
+        }else if (day === 'saturday' || day === 'sunday'){
+                console.log('Enjoy the weekend');
+
+        }else{
+                console.log('Not a valid day')
+        }
+         */
+
+        // Statements and expressions
+
+        // The conditional (ternary) operator
+
+        const age = 20;
+
+        age <= 18 ? console.log("i like to drink whine"):
+        console.log('i like to drink water')
+
+
+
+
+
+         
+
+
+        
+         
+
+        
+
+
+ 
+
+
+
+        
+
+
+
+
+
+
+        
+
+
+
 
 
 
